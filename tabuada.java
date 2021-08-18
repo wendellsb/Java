7a58c2fc42797bfca710d192922fe3ce4792aa14
wendellsb <88899145+wendellsb@users.noreply.github.com>
2021-08-18 20:39:39 -0300

    tabuada.java

---
+++ tabuada.java
@@ -0,0 +1,23 @@
+/*
+    Wendell Santos
+    Mat: 10881
+*/
+class Tabuada {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i + " x 10 = " + i*10);
+        }
+    }
+}
+/*        saída
+        1 x 10 = 10
+        2 x 10 = 20
+        3 x 10 = 30
+        4 x 10 = 40
+        5 x 10 = 50
+        6 x 10 = 60
+        7 x 10 = 70
+        8 x 10 = 80
+        9 x 10 = 90
+        10 x 10 = 100
+*/
